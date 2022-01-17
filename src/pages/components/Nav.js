76e7c54b93b22a292fe9172de0f625aefd8cb1e4
Alex Dring <alexdringppc@gmail.com>
2022-01-17 12:56:07 +0000

    Turn Logo into separate component.

--- src/pages/components/Nav.js
+++ src/pages/components/Nav.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import styled from 'styled-components';
+import Logo from './elements/Logo';
 
 const NavStyles = styled.nav`
   position: fixed;
@@ -37,53 +38,10 @@ const NavStyles = styled.nav`
   }
 `;
 
-const LogoStyles = styled.div`
-  position: relative;
-  div {
-    width: 50px;
-    height: 50px;
-    font-size: 1.125rem;
-    letter-spacing: 1px;
-    transition: background 0.3s, all 0.3s ease-out;
-  }
-  div:first-of-type {
-    display: flex;
-    background: white;
-    align-items: center;
-    justify-content: center;
-    font-family: var(--font-header);
-    font-weight: 700;
-    font-size: 1.125;
-    position: relative;
-    border: solid 2px var(--black);
-    z-index: 1;
-  }
-  div:last-of-type {
-    background: var(--gradient-red);
-    position: absolute;
-    top: 3px;
-    left: 3px;
-  }
-  :hover {
-    div:first-of-type {
-      background: var(--gradient-light-red);
-      /* color: white; */
-      /* border: none; */
-    }
-    div:last-of-type {
-      top: 0px;
-      left: 0px;
-    }
-  }
-`;
-
 export default function Nav() {
   return (
     <NavStyles>
-      <LogoStyles>
-        <div>AD</div>
-        <div />
-      </LogoStyles>
+      <Logo />
       <ul>
         <li>About me</li>
         <li>Work</li>
---
+++ src/pages/components/elements/Logo.js
@@ -0,0 +1,51 @@
+import * as React from 'react';
+import styled from 'styled-components';
+
+const LogoStyles = styled.div`
+  position: relative;
+  div {
+    width: 50px;
+    height: 50px;
+    font-size: 1.125rem;
+    letter-spacing: 1px;
+    transition: background 0.3s, all 0.3s ease-out;
+  }
+  div:first-of-type {
+    display: flex;
+    background: white;
+    align-items: center;
+    justify-content: center;
+    font-family: var(--font-header);
+    font-weight: 700;
+    font-size: 1.125;
+    position: relative;
+    border: solid 2px var(--black);
+    z-index: 1;
+  }
+  div:last-of-type {
+    background: var(--gradient-red);
+    position: absolute;
+    top: 3px;
+    left: 3px;
+  }
+  :hover {
+    div:first-of-type {
+      background: var(--gradient-light-red);
+      /* color: white; */
+      /* border: none; */
+    }
+    div:last-of-type {
+      top: 0px;
+      left: 0px;
+    }
+  }
+`;
+
+export default function Logo() {
+  return (
+    <LogoStyles>
+      <div>AD</div>
+      <div />
+    </LogoStyles>
+  );
+}
